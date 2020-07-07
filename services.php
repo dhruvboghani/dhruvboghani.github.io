45cff2e827dfbff75bc7932e770c7d06b07e88c7
dhruvboghani <ramboghani726@gmail.com>
2020-07-07 14:05:55 +0530

    publishing my website

---
+++ services.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>My Website</title>
+</head>
+<body>
+  <!-- Header -->
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="index.html"><h1><span>S</span>ervices</h1></a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger"><div class="bar"></div></div>
+          <ul>
+            <li><a href="index.html" data-after="Home">Home</a></li>
+            <li><a href="services.php" data-after="Service">Services</a></li>
+            <li><a href="project.php" data-after="Projects">Projects</a></li>
+            <li><a href="about.php" data-after="About">About</a></li>
+            <li><a href="contact.php" data-after="Contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+ 
+ <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1>Hello, <span></span></h1>
+        <h1>My Name is <span></span></h1>
+        <h1>Dhruv <span></span></h1>
+        <a href="project.php" type="button" class="cta">Portfolio</a>
+      </div>
+    </div>
+  </section>
+
+    <section id="services">
+    <div class="services container">
+      <div class="service-top">
+        <h1 class="section-title">Serv<span>i</span>ces</h1>
+        <p> </p>
+      </div>
+      <div class="service-bottom">
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png"/></div>
+          <h2>Web-site Developer</h2>
+          <p></p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png"/></div>
+          <h2>Software Developer</h2>
+          <p></p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png"/></div>
+          <h2>Application Developer</h2>
+          <p></p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png"/></div>
+          <h2>Q & A Testing </h2>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand"><h1><span>D</span>hruv <span>B</span>oghani</h1></div>
+     
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="https://www.facebook.com/ram.boghani.5"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png"/></a>
+        </div>
+        <div class="social-item">
+          <a href="https://www.instagram.com/dhruv_boghani/"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png"/></a>
+        </div>
+        <div class="social-item">
+          <a href="https://twitter.com/DhruvBoghani"><img src="https://img.icons8.com/bubbles/100/000000/twitter.png"/></a>
+        </div>
+        
+      </div>
+      
+    </div>
+  </section>
+
+
+
+  <script src="./app.js"></script>
+</body>
+</html>
